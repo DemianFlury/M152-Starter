303b389e5c722b6e16f33a45d64d38896e78a680
Demian Flury <74318556+DemianFlury@users.noreply.github.com>
2023-04-26 11:47:09 +0200

    start ex 2

--- src/router/index.js
+++ src/router/index.js
@@ -1,6 +1,7 @@
 import { createRouter, createWebHistory } from 'vue-router'
 import HomeView from '../views/HomeView.vue'
 import aufgabe1 from '../views/Pages/aufgabe1.vue'
+import aufgabe2 from '../views/Pages/aufgabe2.vue'
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
@@ -14,6 +15,11 @@ const router = createRouter({
       path: '/aufgabe1',
       name: 'Aufgabe 1',
       component: aufgabe1,
+    },
+    {
+      path: '/aufgabe2',
+      name: 'Aufgabe 2',
+      component: aufgabe2,
     }
   ]
 })
